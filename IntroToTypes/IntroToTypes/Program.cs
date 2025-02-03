85fa571909c82d9991d63157f399bcb37bcc4c9b
türkay ürkmez <turkay.urkmez@outlook.com>
2025-02-03 10:58:04 +0300

    first commit

---
+++ IntroToTypes/IntroToTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz (0...255)");
+byte first = byte.Parse(Console.ReadLine());
+Console.WriteLine("Bir sayı daha giriniz (0...255)");
+
+byte second = byte.Parse(Console.ReadLine());
+
+checked
+{
+    byte total = (byte)(first + second);
+    Console.WriteLine(total);
+}
