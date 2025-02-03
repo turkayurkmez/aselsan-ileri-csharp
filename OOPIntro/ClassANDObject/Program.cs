50418cb9b3dcda9835e60748136b9e7f120a28e7
türkay ürkmez <turkay.urkmez@outlook.com>
2025-02-03 14:00:39 +0300

    new oop

---
+++ OOPIntro/ClassANDObject/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassANDObject;
+
+Console.WriteLine("Hello, World!");
+Musteri ismail = new Musteri();
+
+ismail.Ad = "İsmail";
+
+Musteri neriman = ismail;
+
+neriman.Ad = "Neriman";
+
+Console.WriteLine(ismail.Ad);
+object a = new Musteri();
+object b = "Bu bir cümle";
+object c = 1;
+object d = 1.5;
+object e = "e";
+
+string cumle = (string)c;
+
+Console.WriteLine(cumle.ToUpper());
+
+object birseyYap(object birSey)
+{
+    return null;
+}
+
+birseyYap("deneme");
+birseyYap(15);
